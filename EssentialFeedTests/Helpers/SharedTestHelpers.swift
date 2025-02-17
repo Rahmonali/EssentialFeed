b5d994e9201225c2c4d867e794f98a07b2306755
Rahmonali <rahmonali1995@gmail.com>
2025-02-17 09:42:28 +0500

    Extract dublicate test helpers into a shared scope

---
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Rahmonali on 17/02/25.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
