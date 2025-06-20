53a0a6aaa7c49ecea94b070d295ea409b0accd59
Rahmonali <rahmonali1995@gmail.com>
2025-06-20 08:12:30 +0500

    Group the files to MVC approach

---
+++ EssentialFeediOS/FeedImageLoader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by RF on 20/06/25.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
