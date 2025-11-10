28c7c154ff7643df2f9e023068bd23eb99614d0d
Rahmonali <rahmonali1995@gmail.com>
2025-11-10 14:21:01 +0500

    Encapsulate CoreDataStore bundle creation into a centralized place to remove duplication and prevent mistakes

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -36,7 +36,6 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         save(feed, with: sutToPerformSave)
         
         expect(sutToPerformLoad, toLoad: feed)
-        
     }
     
     func test_save_overridesItemsSavedOnASeparateInstance() {
@@ -55,9 +54,8 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
-        let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = testSpecificStoreURL()
-        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let store = try! CoreDataFeedStore(storeURL: storeURL)
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -66,13 +64,11 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
-        
         loader.save(feed) { result in
             if case let Result.failure(error) = result {
                 XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
             }
             saveExp.fulfill()
-            
         }
         wait(for: [saveExp], timeout: 1.0)
     }
@@ -93,7 +89,6 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    
     private func setupEmptyStoreState() {
         deleteStoreArtifacts()
     }
@@ -113,4 +108,5 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
+    
 }
