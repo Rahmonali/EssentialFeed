7b8981e487d8959f4f0cea11d88fdd3a8619b68c
Rahmonali <rahmonali1995@gmail.com>
2025-11-10 14:03:12 +0500

    Add new FeedStore core data model version adding an optional Data property to the `ManagedFeedImage`

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -8,40 +8,41 @@
 import CoreData
 
 @objc(ManagedCache)
- class ManagedCache: NSManagedObject {
+class ManagedCache: NSManagedObject {
     @NSManaged  var timestamp: Date
     @NSManaged  var feed: NSOrderedSet
 }
 
 extension ManagedCache {
-     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
     }
     
-     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context).map(context.delete)
         return ManagedCache(context: context)
     }
     
-     var localFeed: [LocalFeedImage] {
+    var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
 }
 
 
 @objc(ManagedFeedImage)
- class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged  var id: UUID
     @NSManaged  var imageDescription: String?
     @NSManaged  var location: String?
     @NSManaged  var url: URL
+    @NSManaged var data: Data?
     @NSManaged  var cache: ManagedCache
 }
 
 extension ManagedFeedImage {
-     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
@@ -52,7 +53,7 @@ extension ManagedFeedImage {
         })
     }
     
-     var local: LocalFeedImage {
+    var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
 }
