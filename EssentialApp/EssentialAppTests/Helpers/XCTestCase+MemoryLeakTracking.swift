cc074a7e2c8d2339b2828a959ac60ad5711ac7e3
Rahmonali <rahmonali1995@gmail.com>
2025-11-19 13:05:06 +0500

    Extract memory leak tracking helper into a shared scope to remove duplication

---
+++ EssentialApp/EssentialAppTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift .swift
+//  EssentialAppTests
+//
+//  Created by RF on 19/11/25.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
